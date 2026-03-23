9d71392fb790f0b93f1756bbbfeaa686ac50dc44
empireyoncar <116581743+empireyoncar@users.noreply.github.com>
2026-03-23 19:03:17 +0000

    creacion de routeswallet.py

--- wallet/backend/wallet_webserver.py
+++ wallet/backend/wallet_webserver.py
@@ -1,34 +1,9 @@
-from flask import Flask, send_from_directory, render_template
-import os
+from flask import Flask
+from routeswallet import wallet_web_bp
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMPLATES_DIR = os.path.join(BASE_DIR, "..", "frontend")
-STATIC_DIR = os.path.join(TEMPLATES_DIR, "static")
+app = Flask(__name__)
 
-app = Flask(
-    __name__,
-    template_folder=TEMPLATES_DIR,
-    static_folder=STATIC_DIR
-)
+app.register_blueprint(wallet_web_bp)
 
-# ============================================================
-# Página principal de Wallet
-# ============================================================
-@app.route("/wallet")
-def wallet_page():
-    return render_template("wallet.html")
-
-
-# ============================================================
-# Archivos estáticos (CSS, JS, imágenes)
-# ============================================================
-@app.route("/static/<path:filename>")
-def static_files(filename):
-    return send_from_directory(STATIC_DIR, filename)
-
-
-# ============================================================
-# Servidor
-# ============================================================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003)

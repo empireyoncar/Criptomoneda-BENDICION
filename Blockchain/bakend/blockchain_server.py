472f31d14a3a8ac637d09135045c5d194d5fd056
empireyoncar <116581743+empireyoncar@users.noreply.github.com>
2026-03-30 20:30:59 +0100

    actualizacion de ipinterna de docker, de blochainserver16

--- Blockchain/bakend/blockchain_server.py
+++ Blockchain/bakend/blockchain_server.py
@@ -208,11 +208,5 @@ def commit_block():
 #   INICIAR SERVIDOR
 # ============================================================
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5004,
-        debug=False,
-        use_reloader=False,
-        trusted_hosts=["*", "blockchain_api:5004", "localhost", "127.0.0.1"]
-    )
+    app.run(host="0.0.0.0", port=5004, debug=False, use_reloader=False)
 
